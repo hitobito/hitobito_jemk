7dca8ffb300ccea358d3df1d92091d272aa1b5d4
Matthias Viehweger <viehweger@puzzle.ch>
2023-05-20 22:20:24 +0200

    Insert license headers

--- app/models/jemk/group.rb
+++ app/models/jemk/group.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+#  Copyright (c) 2012-2023, Jungschar EMK. This file is part of
+#  hitobito_jemk and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_jemk.
+
+
 module Jemk::Group
   extend ActiveSupport::Concern
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+#  Copyright (c) 2012-2023, Jungschar EMK. This file is part of
+#  hitobito_jemk and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_jemk.
+
+
 load File.expand_path('../../app_root.rb', __FILE__)
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
