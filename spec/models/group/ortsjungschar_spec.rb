8a8132e974006755db2ab16bb26fe8134349ea26
Matthias Viehweger <viehweger@puzzle.ch>
2023-07-12 13:47:51 +0200

    Specs for wagon-specific camp-integration

---
+++ spec/models/group/ortsjungschar_spec.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+#  Copyright (c) 2023-2023, Jungschar EMK. This file is part of
+#  hitobito_jemk and licensed under the Affero General Public License version 3
+#  or later. See the COPYING file at the top-level directory or at
+#  https://github.com/hitobito/hitobito_jemk.
+
+require 'spec_helper'
+
+describe Group::Ortsjungschar do
+  it 'supports events' do
+    expect(described_class.event_types).to include(Event)
+  end
+
+  it 'supports camps' do
+    expect(described_class.event_types).to include(Event::Camp)
+  end
+end
